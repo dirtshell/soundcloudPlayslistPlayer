bd972c8b16b34d6cf32621b6f95e48e81485e0b8
dirtshell <j.kenin@gmail.com>
2014-08-10 18:56:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ SC.initialize({
 
 $(document).ready(function() {
   SC.get('/tracks/293', function(track) {
-    SC.oEmbed(track.permalink_url, document.getElementById('tracks'));
-  });
+    SC.oEmbed(track.permalink_url, document.getElementById('player'));
+  })
 });
